c5ce4cb0f791213eec09da603a4eda2f34db7ff6
derailed <fernand.galiana@gmail.com>
2026-04-13 11:03:24 -0600

    Updating formula for v0.20.2

--- Formula/isopolicy.rb
+++ Formula/isopolicy.rb
@@ -5,19 +5,19 @@
 
 class Isopolicy < Formula
   desc "Isovalent Network Policy Recommendations"
-  version "v0.20.1"
+  version "v0.20.2"
 
   on_macos do
     if Hardware::CPU.intel?
       url "https://github.com/isovalent/isopolicy/releases/download/" + version + "/isopolicy_darwin_amd64.tar.gz"
-      sha256 "7b30b3eb78ebbb023da8f49ba72ead058104ff2ed5d8eb70567e81cc19525aa0"
+      sha256 "d3651e43793f6474bf6f6ad446c82aded75ffe99f4a3e4d91dd91de241c72956"
       def install
         bin.install "isopolicy"
       end
     end
     if Hardware::CPU.arm?
       url "https://github.com/isovalent/isopolicy/releases/download/" + version + "/isopolicy_darwin_arm64.tar.gz"
-      sha256 "52a83efa27445340b19bdd572094564b06ce919d0bfc122f009c110797d1e6b6"
+      sha256 "308d8463f4acf38f70162cda85abb13b5130583bce9dd4b32c28d65791fc657f"
       def install
         bin.install "isopolicy"
       end
@@ -27,14 +27,14 @@ class Isopolicy < Formula
   on_linux do
     if Hardware::CPU.intel?
       url "https://github.com/isovalent/isopolicy/releases/download/" + version + "/isopolicy_linux_amd64.tar.gz"
-      sha256 "96e3fec48ecf0e175b7ebad22796db3e06aa6c52cf644ce018cb5bb0e08c8fcb"
+      sha256 "ba1c5843efefe2379f074a970690ebad80a895d4a8666542df75307f03ab3f83"
       def install
         bin.install "isopolicy"
       end
     end
     if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
       url "https://github.com/isovalent/isopolicy/releases/download/" + version + "/isopolicy_linux_arm64.tar.gz"
-      sha256 "6081a689f9a23cc0676dfe680e1005d1ae927987061c6e3a3603ffdf30b639d1"
+      sha256 "34cc6bfcfc8547ff44a5686857cd445dc815c96389ae321948dcd4484f0e48e3"
       def install
         bin.install "isopolicy"
       end
